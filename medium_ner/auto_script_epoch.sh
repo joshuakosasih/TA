6e17a8d2e1276be2a88debc62025fd2170aee29b
joshuakosasih <joshua.kosasih@gmail.com>
2018-04-03 23:52:29 +0700

    update auto epoch for no transfer

--- medium_ner/auto_script_epoch.sh
+++ medium_ner/auto_script_epoch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "seed 0"
-python auto_transfer_main_noweighting_dev.py ner_3_train.ner 0 200 16 >> logETnW3.txt
+python auto_main_noweighting_dev.py ner_3_train.ner 0 200 16 >> logEnTnW3.txt
 echo "seed 1"
-python auto_transfer_main_noweighting_dev.py ner_3_train.ner 1 200 16 >> logETnW3.txt
+python auto_main_noweighting_dev.py ner_3_train.ner 1 200 16 >> logEnTnW3.txt
 echo "seed 2"
-python auto_transfer_main_noweighting_dev.py ner_3_train.ner 2 200 16 >> logETnW3.txt
+python auto_main_noweighting_dev.py ner_3_train.ner 2 200 16 >> logEnTnW3.txt
 echo "done!"
