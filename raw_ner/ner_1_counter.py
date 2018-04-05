528a4feff9be099508383357dd7a5aad98450cc6
joshuakosasih <joshua.kosasih@gmail.com>
2018-04-05 10:35:48 +0700

    rapih2kan

---
+++ raw_ner/ner_1_counter.py
@@ -0,0 +1,40 @@
+import os
+
+myfile = open('ner_1_test.txt', 'r')
+
+netype = []
+necnt = []
+mydict = []
+enx = '<ENAMEX TYPE='
+eny = '>'
+
+for line in myfile:
+    mydict.append(line)
+
+
+def updateNEType(s):
+    found = False
+    for idx, x in enumerate(netype):
+        if s == x:
+            found = True
+            necnt[idx] = necnt[idx]+1
+    if not found:
+        netype.append(s)
+        necnt.append(1)
+
+def enamexCounter(s):
+    count = 0
+    while enx in s:
+        count = count + 1
+	s = s[s.find(enx):]
+	updateNEType(s[s.find(enx) + len(enx):s.find(eny)])
+        s = s[len(enx):]
+    return count
+
+sums = 0
+for line in mydict:
+    sums = sums + enamexCounter(line)
+
+print "Sums", sums
+print "NE type", netype
+print "NE count", necnt
