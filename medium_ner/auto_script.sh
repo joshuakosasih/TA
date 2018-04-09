f87cfcd164200277b5cbcb0c4b45bb359b2bb0d0
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-04-09 21:41:38 +0700

    update autos cut

--- medium_ner/auto_script.sh
+++ medium_ner/auto_script.sh
@@ -1,63 +1,91 @@
 #!/bin/bash
-echo "Transfer data 100% - 10%"
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 13 >> logCT_e5.txt
+echo "Transfer data 100%"
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 12 >> logCTfull1.txt
+echo "Transfer data 50% - 10%"
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 12 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 12 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 12 >> logCTfull1.txt
 echo "Transfer data 5% - 1%"
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 14 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 14 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 14 >> logCT_e5.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 12 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 12 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 12 >> logCTfull1.txt
 echo "Transfer data 0.5%"
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 11 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 12 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 13 >> logCT_e5.txt
-python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 14 >> logCT_e5.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 0 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 2 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 3 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 7 >> logCTfull1.txt
+python auto_transfer_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 12 >> logCTfull1.txt
 echo "Transfer done!"
-echo "No transfer data 100% - 10%"
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 13 >> logCnT_e5.txt
-echo "No transfer data 5% - 1%"
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 14 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 14 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 14 >> logCnT_e5.txt
-echo "No transfer data 0.5%"
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 11 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 12 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 13 >> logCnT_e5.txt
-python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 14 >> logCnT_e5.txt
-echo "No transfer done!"
+echo "Non-Transfer data 100%"
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 1 12 >> logCnTfull1.txt
+echo "Non-Transfer data 50% - 10%"
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.5 12 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.2 12 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.1 12 >> logCnTfull1.txt
+echo "Non-Transfer data 5% - 1%"
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.05 12 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.02 12 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.01 12 >> logCnTfull1.txt
+echo "Non-Transfer data 0.5%"
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 0 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 2 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 3 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 7 >> logCnTfull1.txt
+python auto_main_noweighting_cut.py ner_3_train.ner ner_3_test.ner 28 16 0.005 12 >> logCnTfull1.txt
+echo "Non-Transfer done!"
