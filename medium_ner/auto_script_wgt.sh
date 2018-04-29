0d9aa51c895081b9cbe3aba687b0980c04431f3e
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-04-29 22:34:36 +0700

    update auto wgt

--- medium_ner/auto_script_wgt.sh
+++ medium_ner/auto_script_wgt.sh
@@ -3,4 +3,5 @@ python auto_transfer_main_noweighting_wgt.py 27 16 nw921 >> logTwgt.txt
 python auto_transfer_main_noweighting_wgt.py 27 16 nw917 >> logTwgt.txt
 python auto_transfer_main_noweighting_wgt.py 27 16 nw915 >> logTwgt.txt
 python auto_transfer_main_noweighting_wgt.py 27 16 lastw910 >> logTwgt.txt
+python auto_transfer_main_noweighting_wgt.py 27 16 lastw905 >> logTwgt.txt
 echo "Transfer notrainable done!"
