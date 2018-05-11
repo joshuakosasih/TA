fb8b113244b56ee9994493e96b345dc1f270a334
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-05-11 12:37:48 +0700

    update scripts

--- welldone_pos/sexm.sh
+++ welldone_pos/sexm.sh
@@ -2,26 +2,10 @@
 echo "Running exm"
 for i in $(seq 1 3)
 do
-  for j in $(seq 0 1)
-  do
-    python Argv_Main.py . polyglot.vec polyglot-char.vec . . . . $i . . . . . . . >> logExM.txt
-  done
-  for j in $(seq 0 1)
-  do
-    python Argv_Main.py . rang_fasttext.vec rang_fasttext-char.vec . . . . $i . . . . . . . >> logExM.txt
-  done
-  for j in $(seq 0 1)
-  do
-    python Argv_Main.py . rang_word2vec.vec rang_word2vec-char.vec . . . . $i . . . . . . . >> logExM.txt
-  done
-  for j in $(seq 0 1)
-  do
-    python Argv_Main.py . wiki.id.vec wiki.id-char.vec . . . . $i . . . . . . . >> logExM.txt
-  done
-  for j in $(seq 0 1)
-  do
-    python Argv_Main.py . WE_w.vec WE_w-char.vec . . . . $i . . . . . . . >> logExM.txt
-  done
+  python Argv_Main.py . polyglot.vec polyglot-char.vec . . . . $i . . . . . . . >> logExM.txt
+  python Argv_Main.py . rang_fasttext.vec rang_fasttext-char.vec . . . . $i . . . . . . . >> logExM.txt
+  python Argv_Main.py . rang_word2vec.vec rang_word2vec-char.vec . . . . $i . . . . . . . >> logExM.txt
+  python Argv_Main.py . wiki.id.vec wiki.id-char.vec . . . . $i . . . . . . . >> logExM.txt
+  python Argv_Main.py . WE_w.vec WE_w-char.vec . . . . $i . . . . . . . >> logExM.txt
 done
 echo "Done!"
-
--- welldone_pos/smer.sh
+++ welldone_pos/smer.sh
@@ -4,10 +4,10 @@ for i in sum mul concat ave
 do
   for j in sum mul concat ave
   do
-      for k in $(seq 1 6)
-      do
-        python Argv_Main.py . . . . . $i . . $j $k . . . . . >> logMer.txt
-      done
+    for k in $(seq 1 6)
+    do
+      python Argv_Main.py . . . . . $i . . $j $k . . . . . >> logMer.txt
+    done
   done
 done
 echo "Done!"
