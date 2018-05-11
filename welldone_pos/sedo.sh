e6857c0d54b54caaeaac84b905d9e5b7a9ebd476
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-05-11 11:48:01 +0700

    update script dropout

--- welldone_pos/sedo.sh
+++ welldone_pos/sedo.sh
@@ -4,7 +4,7 @@ for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
   for j in $(seq 0 1)
   do
-    python Argv_Main.py . . . . $i . . . . . . . . . . >> logTxM.txt
+    python Argv_Main.py . . . . $i . . . . . . . . . . >> logEDo.txt
   done
 done
 echo "Done!"
--- welldone_pos/sgdo.sh
+++ welldone_pos/sgdo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Running edo"
+echo "Running gdo"
 for i in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
   for j in $(seq 0 1)
   do
-    python Argv_Main.py . . . . . . $i . . . . . . . . >> logTxM.txt
+    python Argv_Main.py . . . . . . $i . . . . . . . . >> logGDo.txt
   done
 done
 echo "Done!"
