8a7033b9706ad3158ca206f8d5c12b896d6a042b
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-05-18 14:00:58 +0700

    add emb vis

--- EmbVisualize/WgtToEmb.py
+++ EmbVisualize/WgtToEmb.py
@@ -1,26 +1,28 @@
 import pickle
 from DataProcessor import DataIndexer as DI
 
-w_name = '05-17_13:37_921'
+w_name = '05-17_22:39_736'
 
-fp = open(w_name + "_5.wgt", "rb")
-fout = open(w_name + ".vec", "w")
+fp = open(w_name + "_1.wgt", "rb")
+fout = open(w_name + "-char.vec", "w")
 w = pickle.load(fp)
 w = w[0]
-word = DI()
-word.load('word')
-ci = word.index
-keys = ci.keys()
-values = ci.values()
+idx = DI()
+idx.load('word')
+ii = idx.index
+keys = ii.keys()
+values = ii.values()
 
 for i, char in enumerate(w):
     if i != 0:
-        c = keys[values.index(i)]
-        try:
-            c.decode('utf-8')
-            fout.write(c)
-            for vec in char:
-                fout.write(' ' + str(vec))
-            fout.write('\n')
-        except UnicodeError:
-            print "char is not UTF-8"
+        if i < idx.cnt:
+            print i
+            c = keys[values.index(i)]
+            try:
+                c.decode('utf-8')
+                fout.write(c)
+                for vec in char:
+                    fout.write(' ' + str(vec))
+                fout.write('\n')
+            except UnicodeError:
+                print "char is not UTF-8"
