9316781f2f0b6f12bc045780c93b34c18906074b
Joshua Kosasih <joshua.kosasih@gmail.com>
2018-05-18 11:49:11 +0700

    update comment on st

--- welldone_ner/st.sh
+++ welldone_ner/st.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Running T cut"
+echo "Running T cut GRU no trainable"
 for i in 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005
 do
   for j in $(seq 0 14)
   do
-    python Argv_MainTransfer_Cut.py $i $j >> logT.txt
+    python Argv_MainTransfer_Cut.py $i $j >> logTg.txt
   done
 done
 echo "Done!"
